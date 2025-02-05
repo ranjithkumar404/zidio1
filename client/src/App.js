813b300750b7eb0bdf3d2832d5139b3ca3ed8658
ranjithkumar404 <ranjithkumarshetty808@gmail.com>
2025-02-05 22:21:39 +0530

    front-end setup

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+   <h1 className="flex justify-center text-4xl items-center"> Hello World </h1>
+    </div>
+  );
+}
+
+export default App;
